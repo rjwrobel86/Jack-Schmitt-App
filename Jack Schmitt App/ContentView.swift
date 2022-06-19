b54b71c66bcbc8050a36ada3e12df927fb38f0e5
Robert Wrobel <rjwrobel86@gmail.com>
2022-06-18 20:03:09 -0500

    lots of stuff - doesn't work

---
+++ Jack Schmitt App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  JSCWR
+//
+//  Created by Bob Wrobel on 6/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Menu()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jack Schmitt App/Jack_Schmitt_AppApp.swift
@@ -0,0 +1,19 @@
+//
+//  Jack_Schmitt_AppApp.swift
+//  Jack Schmitt App
+//
+//  Created by Robert Wrobel on 5/26/22.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct Jack_Schmitt_AppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
